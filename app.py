5fbe1e8b933b253d9b223c98ca9531c8d769521f
khalidsalehali30 <khalidsalehali30@gmail.com>
2026-03-14 17:59:01 +0100

    My CA

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from datetime import datetime
+
+app = Flask(__name__)
+
+questions = [
+    {"question": "What is 9 / 3?", "answer": "3"},
+    {"question": "Capital of Nigeria?", "answer": "abuja"},
+    {"question": "HTML stands for?", "answer": "html"}
+]
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    if request.method == "POST":
+        score = 0
+        
+        for i, q in enumerate(questions):
+            user_answer = request.form.get(f"q{i}")
+            
+            if user_answer.lower() == q["answer"]:
+                score += 1
+
+        time = datetime.now()
+
+        return render_template("result.html", score=score, total=len(questions), time=time)
+
+    return render_template("index.html", questions=questions)
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ question.py
@@ -0,0 +1,14 @@
+questions = [
+    {
+        "question": "What does CPU stand for?",
+        "answer": "central processing unit"
+    },
+    {
+        "question": "Which language is used for web pages?",
+        "answer": "html"
+    },
+    {
+        "question": "What is 5 + 3?",
+        "answer": "8"
+    }
+]
