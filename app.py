aa4beb61290511f4231c6e2f0cfcb3526633c475
khalidsalehali30 <khalidsalehali30@gmail.com>
2026-03-16 14:42:05 +0100

    Modify

--- app.py
+++ app.py
@@ -7,9 +7,9 @@ app = Flask(__name__)
 questions = [
     {"question": "What is 6 - 3?", "answer": "3"},
     {"question": "Capital of Nigeria?", "answer": "abuja"},
-    {"question": "HTML stands for?", "answer": "HyperText Markup Language"},
-    {"question": "What is pwd command?", "answer": "Showing the location"},
-    {"question": "which country has more population in Africa?", "answer": "Nigeria"}
+    {"question": "HTML stands for?", "answer": "hypertext markup language"},
+    {"question": "What is pwd command?", "answer": "print working directory"},
+    {"question": "which country has more population in Africa?", "answer": "nigeria"}
 ]
 
 @app.route("/", methods=["GET", "POST"])
